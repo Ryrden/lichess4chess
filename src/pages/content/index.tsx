18e639035e3315a6b4f8016df4e3f001676fba0e
Ryan S <ryanssteles@gmail.com>
2025-05-03 22:18:56 -0300

    feat: MVP done

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,20 +1,42 @@
-import { createRoot } from 'react-dom/client';
-import './style.css' 
+import axios from 'axios';
+import Browser from 'webextension-polyfill';
+import './style.css';
 const div = document.createElement('div');
 div.id = '__root';
 document.body.appendChild(div);
 
 const rootContainer = document.querySelector('#__root');
 if (!rootContainer) throw new Error("Can't find Content root element");
-const root = createRoot(rootContainer);
-root.render(
-  <div className='absolute bottom-0 left-0 text-lg text-black bg-amber-400 z-50'  >
-    content script <span className='your-class'>loaded</span>
-  </div>
-);
 
 try {
   console.log('content script loaded');
 } catch (e) {
   console.error(e);
 }
+
+Browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  console.log("Received message:", message);
+  console.log("BG page received message", message, "from", sender);
+  if (message.action === "doSomething") {
+    // TODO: Improve this code by using some kind of MutationObserver to detect when the share button is available
+    const shareButton = document.querySelector('.share')
+    shareButton?.click()
+    // Wait for the share menu to open
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the timeout as needed
+    const pgnTab = document.querySelector('#tab-pgn')
+    pgnTab?.click()
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the timeout as needed
+    const pgn = document.querySelector('.share-menu-tab-pgn-textarea')
+    console.log("PGN:", pgn.value)
+
+    const response = await axios.post('https://lichess.org/api/import', {
+      pgn: pgn.value
+    })
+    if (response.status === 200) {
+      const url = response.data.url
+      window.open(url, '_blank')
+    }
+    return sendResponse({ success: true });
+  }
+  return sendResponse({ success: false });
+});
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import { useEffect, useState } from "react";
+import Browser from "webextension-polyfill";
 
 export default function Popup() {
+  const [message, setMessage] = useState("");
+
+  const handleClick = async () => {
+    console.log("Button clicked!");
+    Browser.runtime.sendMessage({ action: "doSomething" });
+    const tabs = await Browser.tabs.query({ active: true, currentWindow: true });
+    console.log("Tabs:", tabs);
+    Browser.tabs.connect(tabs[0].id!,{ name: "popup" });
+    const activeTab = tabs[0];
+    if (activeTab) {
+      await Browser.tabs.sendMessage(activeTab.id!, { 
+        action: "doSomething" 
+      });
+    }
+
+  };
+
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
+      {/* TODO: Button that goes to the options page */}
+      {/* Must be on upper right corner and should be a gear wheel */}
       <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
+        {/* LOGO */}
+        <p>Lichess Analyzer for Chess.com</p>
       </header>
+      <section>
+        {message}
+        <button
+          onClick={handleClick}
+          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded cursor-pointer"
+        >
+          Go to Lichess Analysis
+        </button>
+      </section>
     </div>
   );
 }
