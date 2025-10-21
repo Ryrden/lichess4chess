2a7d5916143829beeedf2638a32744d75a43cd8b
Utsav Jana <utsav.jana.5@gmail.com>
2025-10-21 23:02:02 +0530

    fixed warning message on settings page

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -84,10 +84,6 @@ export default function Options() {
     }, 3000);
   };
 
-  const handleCancelChanges = () => {
-    setSettings(originalSettings);
-    applyTheme(originalSettings.theme);
-  };
   
   const handleToggle = (key: keyof Settings) => {
     if (typeof settings[key] === 'boolean') {
@@ -251,7 +247,7 @@ export default function Options() {
               <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.725-1.36 3.49 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
               </svg>
-              You have unsaved changes. Click "Save Settings" to apply them or "Cancel Changes" to revert.
+              You have unsaved changes. Changes will not be implemented unless "Save Settings" is clicked.
             </div>
           </div>
         )}
@@ -263,13 +259,6 @@ export default function Options() {
             className="px-6 py-2 bg-green-600 text-white font-medium rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
           >
             {getMessage("saveSettings")}
-          </button>
-          
-          <button
-            onClick={handleCancelChanges}
-            className="px-6 py-2 bg-orange-500 text-white font-medium rounded-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-          >
-            Cancel Changes
           </button>
           
           <button
